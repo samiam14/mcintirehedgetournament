d4202bd0492b8a8357b86ecd81abcd18568f3e6f
Sam Eberspacher <see3ya@virginia.edu>
2013-10-07 21:41:52 -0400

    Added historical quotes and latest market quote tracking.

--- src/edu/virginia/mcintire/hedgetournament/securities/Security.java
+++ src/edu/virginia/mcintire/hedgetournament/securities/Security.java
@@ -2,6 +2,7 @@ package edu.virginia.mcintire.hedgetournament.securities;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import edu.virginia.mcintire.hedgetournament.trading.Quote;
@@ -9,13 +10,25 @@ import edu.virginia.mcintire.hedgetournament.trading.Quote;
 public abstract class Security {
     protected String _symbol;
     protected Map<Date, Quote> _quotes;
+    protected Quote _marketQuote;
 
     public Security(String symbol) {
 	_quotes = new HashMap<Date, Quote>();
 	_symbol = symbol;
     }
+    
+    public Security(String symbol, List<Quote> historicalQuotes) {
+	_symbol = symbol;
+	_quotes = new HashMap<Date, Quote>();
+	for(Quote q : historicalQuotes) {
+	    _quotes.put(q.getDate(), q);
+	}
+    }
 
     public void addQuote(Quote quote) {
+	if(quote.getDate().after(_marketQuote.getDate())) {
+	    _marketQuote = quote;
+	}
 	_quotes.put(quote.getDate(), quote);
     }
 
@@ -25,6 +38,10 @@ public abstract class Security {
 
     public Quote getQuote(Date date) {
 	return _quotes.get(date);
+    }
+    
+    public Quote getMarketQuote() {
+	return _marketQuote;
     }
 
     public Map<Date, Quote> getQuotes() {
